79e79d05ef4bab36ca9136add64a5cd97e634763
Jubair02 <104502849+Jubair02@users.noreply.github.com>
2025-01-21 20:03:28 +0600

    Add project files.

---
+++ EmployeeAdminPanal/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeAdminPanal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPanal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        //{
+
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }  
+
+    }
+    
+    
+}
---
+++ EmployeeAdminPanal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeAdminPanal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
